107543c97b7fb9c1fc031f6443505ec2252363e3
keviin999 <kevindorado2811@gmail.com>
2023-05-31 20:16:55 -0300

    calculadora final edition

--- public/main.js
+++ public/main.js
@@ -6,9 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function mostrarcal() {
   let caldiarias = localStorage.getItem("caldiarias");
+  let deficit = localStorage.getItem("deficit");
+  let superavit = localStorage.getItem("superavit");
+
   if (caldiarias != null) {
     let vercaldiarias = document.getElementById("vercaldiarias");
+    let verdeficit = document.getElementById("verdeficit");
+    let versuperavit = document.getElementById("versuperavit");
+    verdeficit.value = deficit;
     vercaldiarias.value = caldiarias;
+    versuperavit.value = superavit;
   }
 }
 
@@ -35,16 +42,26 @@ function calculadora() {
     localStorage.setItem("actividad", actividad);
 
     if (hombre) {
+      //calculo Hombre es tu (tasa metabólica basal * factor de actividad * termogenesis)
       let calculohombre =
-        (66 + 13.7 * peso + 5 * altura - 6.8 * edad) * actividad;
-      console.log("calculohombre es" + calculohombre);
+        (66 + 13.7 * peso + 5 * altura - 6.8 * edad) * actividad * 1.1;
+      //Para superavit le sumas entre 400 y 800 (principiante) y intermedio entre 200 y 500kcal, asi que redondeo en 500
+      let superavit = calculohombre + 500;
+      //Para deficit le restas entre 200 y 500, asi que redondeo a 300
+      let deficit = calculohombre - 300;
       localStorage.setItem("caldiarias", Math.floor(calculohombre));
+      localStorage.setItem("superavit", Math.floor(superavit));
+      localStorage.setItem("deficit", Math.floor(deficit));
       mostrarcal();
     } else {
+      //calculo Mujer es tu (tasa metabólica basal * factor de actividad * termogenesis)
       let calculomujer =
         (655 + 9.6 * peso + 1.8 * altura - 4.7 * edad) * actividad;
-      console.log("calculomujer es" + calculomujer);
+      let superavit = calculomujer + 500;
+      let deficit = calculomujer - 300;
       localStorage.setItem("caldiarias", Math.floor(calculomujer));
+      localStorage.setItem("superavit", Math.floor(superavit));
+      localStorage.setItem("deficit", Math.floor(deficit));
       mostrarcal();
     }
   } else {
